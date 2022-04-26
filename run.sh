a1f4286a12c3e64eb3cb8c44c2dac92d02588869
Erick Palacios Moreno <palmoreck@gmail.com>
2022-04-26 16:36:04 -0500

    using https

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone --single-branch -b sinergia git@github.com:palmoreck/talleres-itam.git sinergia
+cd ${HOME} && git clone --single-branch -b sinergia https://github.com/palmoreck/talleres-itam.git sinergia
 exec "$@"
